e092c4162ba1eec945e1252ca81870fb69137d06
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-05 01:39:45 +0700

    re-fix cors

--- src/api/app.js
+++ src/api/app.js
@@ -6,6 +6,7 @@ import __Chat from "./routes/chat.route";
 import __User from "./routes/user.route";
 import { activateAccount } from "./controllers/user.controller";
 import path from "path";
+import allowCors from "../config/cors.config";
 
 const app = express();
 
@@ -25,22 +26,7 @@ app.use(
 		threshold: 10 * 1000,
 	}),
 );
-app.use(async (req, res, next) => {
-	res.setHeader("Access-Control-Allow-Credentials", true);
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	// another common pattern
-	res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-	res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-	);
-	if (req.method === "OPTIONS") {
-		res.send({ status: 200, message: "ok" });
-	} else {
-		next();
-	}
-});
+app.use(allowCors);
 app.use(morgan("tiny"));
 app.use("/api", __Chat);
 app.use("/api", __User);
