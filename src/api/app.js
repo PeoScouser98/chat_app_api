03323cbff7bff97c0ddcdcec91435221f1c1bd92
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-05 03:02:35 +0700

    try to fix cors

--- src/api/app.js
+++ src/api/app.js
@@ -13,8 +13,19 @@ app.use(express.json());
 
 app.use(cors({ origin: "*" }));
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+	);
+	if (req.method === "OPTIONS") {
+		res.status(200).end();
+		return;
+	}
 	next();
 });
 
