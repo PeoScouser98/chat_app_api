1f9fecad19c100464aaf625402578c83933a94a6
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-05 02:14:40 +0700

    Access-Control-Allow-Origin with response header

--- src/api/app.js
+++ src/api/app.js
@@ -20,6 +20,12 @@ app.use(
 		optionsSuccessStatus: 200,
 	}),
 );
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use(
 	compression({
 		level: 6,
