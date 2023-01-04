7415419a590911b600d44564a32cfcc3088e1b24
Quang Hiệp <quanghiep03198@gmail.com>
2023-01-05 01:17:14 +0700

    update cors config

--- src/api/app.js
+++ src/api/app.js
@@ -6,7 +6,6 @@ import __Chat from "./routes/chat.route";
 import __User from "./routes/user.route";
 import { activateAccount } from "./controllers/user.controller";
 import path from "path";
-import allowCors from "../config/cors.config";
 
 const app = express();
 
@@ -26,7 +25,19 @@ app.use(
 		threshold: 10 * 1000,
 	}),
 );
-app.use(allowCors);
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	// another common pattern
+	res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+	res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+	);
+
+	next();
+});
 app.use(morgan("tiny"));
 app.use("/api", __Chat);
 app.use("/api", __User);
